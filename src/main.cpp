affc613d001e6f81291fa65a20f65000437dc4ec
Rodrigo Setti <rodrigosetti@gmail.com>
2015-01-27 17:45:51 -0800

    Setup glfw boiler-plate: black window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,52 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+static void error_callback (int error, const char* description) {
+    std::cerr << description << std::endl;
 }
+
+static void key_callback (GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+        return;
+    }
+}
+
+int main (void) {
+    GLFWwindow* window;
+    float lastTime = 0;
+
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "React Diffuse", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        double time = glfwGetTime();
+        // delta t = time - lastTime
+        lastTime = time;
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
+
