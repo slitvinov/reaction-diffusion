2d07a80d57253b2ffdd40662cee4d2ad36f6e149
Sergey Litvinov <slitvinov@gmail.com>
2021-01-30 00:28:32 +0100

    one file

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 .POSIX:
 .SUFFIXES:
 
-CFLAGS = -Og -g
+CFLAGS = -O2 -g
 all: main
 SDL_FLAGS = `pkg-config --libs --cflags sdl`
-0 = main.c reactdiffuse.c
+0 = main.c
 main: $0
 	$(CC) $0 $(CFLAGS) $(SDL_FLAGS) -lm $(LDFLAGS) -o $@
 clean:
--- main.c
+++ main.c
@@ -1,6 +1,104 @@
 #include <SDL.h>
 #include <stdio.h>
-#include "reactdiffuse.h"
+#include <math.h>
+#include <stdlib.h>
+
+#define DU 1.0
+#define DV 0.5
+
+typedef struct {
+    size_t width;
+    size_t height;
+    double f;
+    double k;
+    double du;
+    double dv;
+
+    double *U;
+    double *V;
+    double *swapU;
+    double *swapV;
+} reaction_diffusion_system ;
+
+#define reaction_diffusion_system_get(s, m, x, y) (m[(((y) + (s)->height) % (s)->height)*(s)->width + (((x) + (s)->width)  % (s)->width)])
+#define reaction_diffusion_system_set(s, m, x, y, v) (m[(((y) + (s)->height) % (s)->height)*(s)->width + (((x) + (s)->width)  % (s)->width)] = fmin(1, fmax(-1, (v))))
+
+void reaction_diffusion_system_init(reaction_diffusion_system *s,
+                                    size_t width,
+                                    size_t height,
+                                    double f,
+                                    double k,
+                                    double du,
+                                    double dv) {
+
+    s->width = width;
+    s->height = height;
+    s->f = f;
+    s->k = k;
+    s->du = du;
+    s->dv = dv;
+    s->U = (double*)malloc(width * height * sizeof(double));
+    s->V = (double*)malloc(width * height * sizeof(double));
+    s->swapU = (double*)malloc(width * height * sizeof(double));
+    s->swapV = (double*)malloc(width * height * sizeof(double));
+
+    // initialize
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            reaction_diffusion_system_set(s, s->U, x, y, 1);
+            reaction_diffusion_system_set(s, s->V, x, y, 0);
+        }
+    }
+}
+
+void reaction_diffusion_system_free(reaction_diffusion_system *s) {
+    free(s->U);
+    free(s->V);
+    free(s->swapU);
+    free(s->swapV);
+}
+
+double get_laplacian(reaction_diffusion_system *s, double *from, int x, int y) {
+    return .05 * reaction_diffusion_system_get(s, from, x-1, y-1) +
+           .2  * reaction_diffusion_system_get(s, from, x-1, y  ) +
+           .05 * reaction_diffusion_system_get(s, from, x-1, y+1) +
+           .2  * reaction_diffusion_system_get(s, from, x  , y-1) +
+           -1. * reaction_diffusion_system_get(s, from, x  , y  ) +
+           .2  * reaction_diffusion_system_get(s, from, x  , y+1) +
+           .05 * reaction_diffusion_system_get(s, from, x+1, y-1) +
+           .2  * reaction_diffusion_system_get(s, from, x+1, y  ) +
+           .05 * reaction_diffusion_system_get(s, from, x+1, y+1);
+}
+
+void reaction_diffusion_system_update(reaction_diffusion_system *s, double dt) {
+    double *temp;
+
+    // calculate new concentrations, and write the new value to swaps
+    for (int x = 0; x < s->width; x++) {
+        for (int y = 0; y < s->height; y++) {
+            double u = reaction_diffusion_system_get(s, s->U, x, y);
+            double v = reaction_diffusion_system_get(s, s->V, x, y);
+            double deltaU = s->du*get_laplacian(s, s->U, x, y)
+                            - (u * v * v)
+                            + s->f*(1. - u);
+            double deltaV = s->dv*get_laplacian(s, s->V, x, y)
+                            + (u * v * v)
+                            - (s->k + s->f)*v;
+
+            reaction_diffusion_system_set(s, s->swapU, x, y, u + deltaU*dt);
+            reaction_diffusion_system_set(s, s->swapV, x, y, v + deltaV*dt);
+        }
+    }
+
+    // swap
+    temp = s->U;
+    s->U = s->swapU;
+    s->swapU = temp;
+
+    temp = s->V;
+    s->V = s->swapV;
+    s->swapV = temp;
+}
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 800
